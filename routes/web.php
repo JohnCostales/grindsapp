48130e7c747f708d2d32052729982090bfe7fe06
JohnCostales <n00151575@student.iadt.ie>
2019-01-14 15:24:00 +0000

    Updated Side Panels and nav tools

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::match(['get', 'post'], '/admin', 'AdminController@login');
+
+Route::get('/logout','AdminController@logout');
+
+Auth::routes();
+
+//Admin middleware. Routes only accissible to logged in users
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/admin/dashboard','AdminController@dashboard');
+    Route::get('/admin/settings','AdminController@settings');
+    Route::get('/admin/check-pwd', 'AdminController@checkPassword');
+    Route::match(['get', 'post'],'/admin/update-pwd','AdminController@updatePassword');
+
+    // Categories routes for Admins
+    Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
+    Route::match(['get', 'post'], '/admin/edit-category/{id}', 'CategoryController@editCategory');
+    Route::match(['get', 'post'], '/admin/delete-category/{id}', 'CategoryController@deleteCategory');
+    Route::get('/admin/view-categories', 'CategoryController@viewCategories');
+
+    // Product routes for Admins
+    Route::match(['get','post'],'/admin/add-product','ProductsController@addProduct');
+    Route::match(['get', 'post'], '/admin/edit-product/{id}', 'ProductsController@editProduct');
+    Route::get('/admin/view-products', 'ProductsController@viewProducts');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
